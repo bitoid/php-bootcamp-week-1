d1548f41a94444fe4394caba22daec933afe7ee3
Giorgadze <ddddgggg933@gmail.com>
2022-05-17 00:48:09 +0400

    Submit week1 HW

---
+++ davit_giorgadze/Challenge #1/README.md
@@ -0,0 +1,23 @@
+#Before you start app.
+
+- Open database.php and change the database environments to your own.
+    ```php
+  $host = "localhost"; // Host name
+    $database = "YOUR_DATABASE"; // Your database name
+    $user = "root"; // Your database user
+    $password = ""; // Your database password
+    
+    
+    ```
+
+- Create users table :
+    ```sql
+    create table users (
+        id int(11) not null auto_increment,
+        first_name varchar(255) not null,
+        last_name varchar(255) not null,
+        primary key (id)
+    );
+    ```
+
+
---
+++ davit_giorgadze/Challenge #2/index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Git</title>
+</head>
+<body>
+<section id="cover" class="min-vh-100">
+    <div id="cover-caption">
+        <div class="container">
+            <div class="row text">
+                <div class="col-xl-5 col-lg-8 col-md-8 col-sm-10 mx-auto text-center form p-4">
+                    <h5 class="display-6 py-2 text-truncate">Enter GitHub Username.</h5>
+                    <div class="px-2">
+                        <form action="about.php" class="justify-content-center" method="post">
+                            <div class="form-group">
+                                <label>
+                                    <label style="font-style: italic" for="username">Username</label>
+                                    <input type="text" id="username" class="form-control" name="username"
+                                           placeholder="GitHub@Username">
+                                </label>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary btn-lg mt-3">Launch</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
