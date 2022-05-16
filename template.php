e78681fe1b46cbdd326840cee73cac181dc44aa6
Amadeuse <giviberdzenishvili@gmail.com>
2022-05-16 23:38:41 +0400

    Add files via upload

---
+++ template.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+    <title>Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/vendor/bs5/css/bootstrap.min.css">
+
+    <!-- Main Style -->
+    <link href="/css/main.css" rel="stylesheet" type="text/css">
+
+    <!-- Scripts -->
+    <script src="/vendor/bs5/js/bootstrap.bundle.min.js"></script>
+    <script src="/vendor/jquery351/jquery-3.5.1.min.js"></script>
+    <script src="/js/main.js"></script>
+</head>
+<body>
+
+Hello World
+
+</body>
+
+</html>
