a66dfc117311c538c6f9a77b9b200a3cabfd5a4e
TsotneForester <tsotne.meladze.usa@gmail.com>
2022-05-18 19:06:05 +0400

    Purify PHP
    removed js type effect, display none method for hidinf for div changed to empty() method

--- tsotne_meladze/week1-1/index.php
+++ tsotne_meladze/week1-1/index.php
@@ -1,26 +1,23 @@
 <?php
-  $display_status = "block";
+
   $pattern = "/^[A-Z]{2,}$/";
   $error = "";
   $divResult = "";
+  $fname = "";
+  $lname = "";
   if (isset($_POST["submit"])) {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     if (!preg_match($pattern, $lname) || !preg_match($pattern, $fname) || $_FILES["uploadfile"]["size"] > 1000000) {
       $error =  "არასწორი ფორმატი, გამოიყენე A-Z და ფაილი 500kb-ზე ნაკლები ზომის";
-      $imageURL = "";
+      $image_name = "x.png";
       $fullName = "";
     }  else  {
-        $display_status = "none";
         $image_name = $_FILES['uploadfile']['name'];
         $image_tmp_name= $_FILES['uploadfile']['tmp_name'];
         move_uploaded_file($_FILES['uploadfile']['tmp_name'], "vault/$image_name");
-        $imageURL = "<img src='vault/$image_name'>";
         $fullName = $fname . " " . $lname;
     }
-
-    $divResult = '<div class="result"><p><span class="auto-input">' . '</span></p>  ' . $imageURL . '<br></div>';
-
   }
 ?>
 
@@ -35,7 +32,6 @@
     <meta property="og:image" content="https://gpx.ge/root/img/main.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
     <link rel="stylesheet" href="css/style.css" />
     <link rel="shortcut icon" type="image/x-icon" href="img/ico.ico" />
    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
@@ -44,45 +40,42 @@
     <title>Profile</title>
   </head>
   <body>
+<?php if (empty($_POST)) {?>
 
-  <div class="box" style="display: <?php echo $display_status ?>;">
-
-
-
+  <div class="box" >
       <form action="" method="post" enctype="multipart/form-data">
         <div class="line1">
           <div class="icon"><i class='bx bx-user-check'></i></div>
           <div class="input"><input id="lname" type="text" name="fname" placeholder="სახელი" required /></div>
         </div>
-
         <div class="line2">
           <div class="icon"><i class='bx bx-user-plus'></i></div>
           <div class="input"><input id="lname" type="text"  name="lname" placeholder="გვარი" required/></div>
         </div>
-
         <div class="line3">
-
           <input type="file" name="uploadfile" value="" required/>
         </div>
-
         <div id="line4">
           <input id="submit" type="submit" name="submit" value="SEND" class="btn btn-success"  />
-
         </div>
       </form>
-    </div>
+  </div>
+  <?php } ?>
 
-    <?php echo $divResult ?>
 
 
-    <script>
-      let typed = new Typed(".auto-input", {
-        strings: ["<?php echo $fullName . $error ?>"],
-        typeSpeed: 80,
-        backSpeed: 250,
-        loop: false,
-      });
-    </script>
+  <?php if (!empty($_POST)) {?>
+  <div class="result">
+    
+    <p><?=$error . $fullName?></p>
+    <img src='vault/<?=$image_name?>'>
+
+  </div>
+  <?php } ?>
+
+
+
+
 
 
 
