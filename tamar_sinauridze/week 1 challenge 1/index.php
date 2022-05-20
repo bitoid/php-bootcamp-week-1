80c267c8a7bc0a2ef506facf814baa9769162924
Tamar Sinauridze <sin165165@gmail.com>
2022-05-20 22:12:34 +0400

    changed code to make it more readable created a separate php file for validations

--- tamar_sinauridze/week 1 challenge 1/index.php
+++ tamar_sinauridze/week 1 challenge 1/index.php
@@ -1,3 +1,10 @@
+<?php
+$errors = [];
+if(!empty($_POST)) {
+    require_once("validations.php");
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,110 +12,34 @@
     </head>
     <body>
         <div class="container">
-            <?php
-            
-            $uploadOk = 0;
+            <?php if(!empty($_POST) && !empty($errors)): ?>
+                <p class="errmsg">Form was not uploaded</p>
+            <?php endif ?>
 
-            if(isset($_POST["submit"])) {
-
-                $uploadOk = 1;
-
-                // validates first name
-                if(empty($_POST["first_name"])) {
-                    $uploadOk = 0;
-                    echo '<p class="errmsg">First name required</p>';
-                } else {
-                    if (!preg_match("/^[a-zA-Z]*$/",$_POST["first_name"])) {
-                        echo '<p class="errmsg">Only English letters allowed</p>';
-                        $uploadOk = 0;
-                    } else {
-                        $_POST["first_name"] = test_input($_POST["first_name"]);
-                    }
-                }
-
-                // validates last name
-                if(empty($_POST["last_name"])) {
-                    $uploadOk = 0;
-                    echo '<p class="errmsg">Last name required</p>';
-                } else {
-                    if (!preg_match("/^[a-zA-Z]*$/",$_POST["last_name"])) {
-                        echo '<p class="errmsg">Only English letters allowed</p>';
-                        $uploadOk = 0;
-                    } else {
-                        $_POST["last_name"] = test_input($_POST["last_name"]);
-                    }
-                }
-
-                // validates picture
-                if(empty($_FILES["userpic"]["name"])) {
-                    echo '<p class="errmsg">Picture required</p>';
-                    $uploadOk = 0;
-                } else {
-                    $check = getimagesize($_FILES["userpic"]["tmp_name"]);
-
-                    // creates uploads folder if it doesn't exist
-                    if (!is_dir("uploads")) {
-                        $oldmask = umask(0);
-                        mkdir("uploads", 0777);
-                        umask($oldmask);
-                    }
-
-                    $target_dir = "uploads/";
-                    $target_file = $target_dir . basename($_FILES["userpic"]["name"]);
-                    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-                    if($check ===false){
-                        echo '<p class="errmsg">File is not an image</p>';
-                        $uploadOk = 0;
-                    } else {
-                        if ($_FILES["userpic"]["size"] > 5242880) {
-                            echo '<p class="errmsg">File is too large</p>';
-                            $uploadOk = 0;
-                        }
-                        if($imageFileType !="jpg" && $imageFileType !="png" && $imageFileType !="jpeg" && $imageFileType !="gif"){
-                            echo '<p class="errmsg">Only JPG, JPEG, PNG & GIF files are allowed</p>';
-                            $uploadOk = 0;
-                        }
-                    }
-                }
-
-                // if form successfully uploaded displays submitted data
-                if($uploadOk==0){
-                    echo '<p class="errmsg">Form was not uploaded</p>';
-                } else {
-                    if(move_uploaded_file($_FILES["userpic"]["tmp_name"], $target_file)) {
-                        echo '<p>Successfully submitted:</p><p>Name:</p><h2>' . $_POST["first_name"] . ' ' . $_POST["last_name"] . '</h2><p>Picture:</p>';
-                        echo '<img src="' . $target_file . '" alt="User image"';
-                    } else {
-                        echo '<p class="errmsg">There was an error uploading your file</p>';
-                    }
-                }
-            }
-
-            // displays form if it's not submitted successfully
-            if($uploadOk==0) {
-                echo <<<TEXT
+            <?php if(empty($_POST) || !empty($errors)): ?>
                 <form action="index.php" method="post" enctype="multipart/form-data">
                     <label for="first-name">First name:</label>
                     <input type="text" id="first-name" name="first_name" placeholder="Enter your first name"
                     pattern="[a-zA-Z]{1,}" title="English letters only" required>
+                    <span class="errmsg"><?php echo $errors["first_name"]?? '' ?></span>
                     <label for="last-name">Last name:</label>
                     <input type="text" id="last-name" name="last_name" placeholder="Enter your last name"
                     pattern="[a-zA-Z]{1,}" title="English letters only" required>
+                    <span class="errmsg"><?php echo $errors["last_name"]?? '' ?></span>
                     <label for="picture">Picture:</label>
                     <input type="file" id="picture" name="userpic" title="Upload JPG, JPEG, PNG or GIF file. Up to 5MB" required>
+                    <span class="errmsg"><?php echo $errors["picture"]?? '' ?></span>
                     <input type="submit" name="submit">
                 </form>
-                TEXT;
-            }
+            <?php endif ?>
 
-            function test_input($data) {
-                $data = trim($data);
-                $data = stripslashes($data);
-                $data = htmlspecialchars($data);
-                return $data;
-            }
-            ?>
+            <?php if (!empty($_POST) && empty($errors)) : ?>
+                <p>Successfully submitted:</p>
+                <p>Name:</p>
+                <h2><?php echo $_POST["first_name"] . ' ' . $_POST["last_name"]?></h2>
+                <p>Picture:</p>
+                <img src="<?php echo $target_file ?>" alt="User image">
+            <?php endif ?>
         </div>
     </body>
 </html>
