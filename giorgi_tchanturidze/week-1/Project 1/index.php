42a537b2f552c4562289b012f69adaddecdae942
Giorgi <you@example.com>
2022-05-20 16:30:05 +0400

    Giorgi Tchanturidze Updated homework for week 1

--- giorgi_tchanturidze/week-1/Project 1/index.php
+++ giorgi_tchanturidze/week-1/Project 1/index.php
@@ -1,4 +1,7 @@
-  <?php
+
+<!--                            Week 1 Project 2                        -->
+
+<?php
   //Cheking name and surname if they are alphabet characters (A to Z)
     if (preg_match("/[^A-Za-z'-]/",$_POST['name'])){
 
@@ -6,13 +9,7 @@
     }
     if (preg_match("/[^A-Za-z'-]/",$_POST['surname'])){
 
-<<<<<<< HEAD
-    if(isset($image)){
-      $imagePath = 'images/'.$image['name'] ;
-      move_uploaded_file($image['tmp_name'],$imagePath);
-=======
       $errors[]='Invalid Last name. <br /> Should be only alphabet characters (A to Z)';
->>>>>>> 381db55 (Giorgi Tchanturidze - week 1 home work (updated))
     }
   ?>
 <!DOCTYPE html>
@@ -68,70 +65,4 @@
 
 </body>
 
-<<<<<<< HEAD
-    <!-- Bootstrap, CSS -->
-    <link href="appcss.css"  rel="stylesheet"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      <h1>Week 1 - Project 1</h1>
-      <!-- If there are any errors, print them -->
-      <?php if (!empty($errors)): ?>
-            <div class="alert alert-danger">
-                <?php foreach($errors as $error): ?>
-                    <div><?php echo $error ?></div>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
-      <!-- Submitting with POST method -->
-      <form method="POST" enctype="multipart/form-data">
-        <div class="form-group">
-          <label >Upload image</label>
-          <input type="file" name="image" />
-        </div>
-        <div class="form-group">
-          <label >Name and Last Name contain only alphabet characters (A to Z)</label>
-          <input type="text" class="form-control" name="name" placeholder="Name" />
-        </div>
-        <div class="form-group">
-          <input type="lastname" class="form-control" name="lastname" placeholder="Last name"  />
-        </div>
-        <div class="form-group">   
-          <button type="submit" class="btn btn-primary" name="upload">Submit</button>      
-        </div>
-      </form>
-      <!-- Bootstrap CSS Table -->
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Image</th>
-            <th scope="col">Name</th>
-            <th scope="col">Lastname</th>
-          </tr>
-        </thead>
-    <tbody>
-      <!-- Showing results if no errors -->
-      <?php if (empty($errors)): ?>
-        <tr>
-          <td>
-            <img src="<?php echo $imagePath?>" class="product-img">
-          </td>
-          <td>
-            <?php echo $name ?>
-          </td>
-          <td>
-            <?php echo $lastname ?>
-          </td>
-        </tr>
-      <?php endif; ?>
-    </tbody>
-      
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
 </html>
-=======
-</html>
->>>>>>> 381db55 (Giorgi Tchanturidze - week 1 home work (updated))
