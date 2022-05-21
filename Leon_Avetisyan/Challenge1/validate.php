0fc40f891879927266f631ec68a71ac161e0f65e
Leon Avetisyan <leon.avetisyan.1993@gmail.com>
2022-05-21 18:10:09 +0400

    Changed variables naming to procedural case(snake case) style

--- Leon_Avetisyan/Challenge1/validate.php
+++ Leon_Avetisyan/Challenge1/validate.php
@@ -2,13 +2,13 @@
     /* Globally declared variables */
 
     // Variables for error texts 
-    $firstNameErr = '';
-    $lastNameErr = '';
-    $fileErr = '';
+    $first_name_err = '';
+    $last_name_err = '';
+    $file_err = '';
 
     // Variables for input data 
-    $firstName = '';
-    $lastName = '';
+    $first_name = '';
+    $last_name = '';
     $image = '';
 
     // Variables for adding styles in html 
@@ -18,24 +18,24 @@
     // At first we check whether submit button clicked or not 
     if(isset($_POST['submit'])) {
         // Assign submitted data values to variables
-        $firstName = trim($_POST['firstName']);
-        $lastName = trim($_POST['lastName']);
+        $first_name = trim($_POST['first_name']);
+        $last_name = trim($_POST['last_name']);
         $image = $_FILES['profImg']['name']; // Here we get image's name
 
-        if(empty($_POST['firstName'])) { // Here we check whether input field for first name filled or not
-            $firstNameErr = 'Please enter your first name'; // And then assign error text
-        }else if(!preg_match("/^[a-z]+$/i", $_POST['firstName'])) { // Here we check whether input data matches to English alphabet
-            $firstNameErr = 'Your first name must contain only English letters'; // And then assign error text
+        if(empty($_POST['first_name'])) { // Here we check whether input field for first name filled or not
+            $first_name_err = 'Please enter your first name'; // And then assign error text
+        }else if(!preg_match("/^[a-z]+$/i", $_POST['first_name'])) { // Here we check whether input data matches to English alphabet
+            $first_name_err = 'Your first name must contain only English letters'; // And then assign error text
         }
 
-        if(empty($_POST['lastName'])) { // Here we check whether input field for last name filled or not
-            $lastNameErr = 'Please enter your last name'; // And then assign error text
-        }else if(!preg_match("/^[a-z]+$/i", $_POST['lastName'])) { // Here we check whether input data matches to English alphabet
-            $lastNameErr = 'Your last name must contain only English letters'; // And then assign error text
+        if(empty($_POST['last_name'])) { // Here we check whether input field for last name filled or not
+            $last_name_err = 'Please enter your last name'; // And then assign error text
+        }else if(!preg_match("/^[a-z]+$/i", $_POST['last_name'])) { // Here we check whether input data matches to English alphabet
+            $last_name_err = 'Your last name must contain only English letters'; // And then assign error text
         }
         
         if($_FILES['profImg']['error'] != 0) { // Here we check whether image's error status equals to zero or not
-            $fileErr = 'Please upload your profile image'; // And then assign error text
+            $file_err = 'Please upload your profile image'; // And then assign error text
         }
     
         // Here we check whether image file uploaded or not for saving in particular destination 
