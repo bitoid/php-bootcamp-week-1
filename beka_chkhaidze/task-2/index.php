8f4bb48210084d4a024bddabb3b30cadab13c382
Beka Chkhaidze <56247764+chkhaidzebeka@users.noreply.github.com>
2022-05-22 17:09:56 +0400

    Update index.php

--- beka_chkhaidze/task-2/index.php
+++ beka_chkhaidze/task-2/index.php
@@ -1,9 +1,11 @@
 <?php
     require_once "./helpers.php";
 
-    $username = $_POST['username'];
-    $sent = (bool)isset($_POST['fetch']);
+    $username = isset($_POST['username'])
+        ? $_POST['username']
+        : @$_SESSION['username'];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,44 +18,16 @@
 <body>
     <main class="container">
         <form action="." method="POST" class="form">
-            <input type="text" name="username" placeholder="username" value="<?php echo $username; ?>" />
+            <input type="text" name="username" placeholder="username" required value="<?= $username; ?>" />
+            <select name="type" id="type">
+                <option value="followers">followers</option>
+                <option value="repos">repos</option>
+            </select>
             <input type="submit" value="fetch" name="fetch" />
         </form>
 
-
-        <div class="summary">
-            <?php
-                if (!empty($username)) {
-                    get_basic_info($username);
-                    $user_exist = check_user_existence();
-                    
-                    if (!$user_exist) {
-                        echo "<div class='error'>user \"" . htmlspecialchars($username, ENT_QUOTES, 'UTF-8') . "\" doesn't exist</div>";
-                    } else {
-                        $repos = fetch_data($username,"repos");
-                        $followers = fetch_data($username,"followers");
-                        // print_r($followers);
-
-                        echo '<p class="intro">Repositories:</p><ul class="repos items">';
-                        foreach($repos as $repo) {
-                            echo "<li class='items__item'><a href='" . $repo->html_url . "' target='blank'>" . $repo->full_name . "</a></li>";
-                        }
-                        echo '</ul>';
-
-                        echo '<p class="intro">Followers:</p><ul class="items followers">';
-                        foreach($followers as $follower) {
-                            echo "<li class='items__item followers__item'>
-                                <a href='" . $follower->html_url . "' target='blank'>
-                                    <img src='" . $follower->avatar_url ." alt='" . $follower->html_url . "'/>
-                                    <p>" . $follower->login . "</p>
-                                </a>
-                            </li>";
-                        }
-                        echo '</ul>';
-                    }
-                }
-            ?>
-        </div>
+        <?php draw_html(); ?>
+        <?php draw_pagination(); ?>
     </main>
 </body>
 </html>
