db2a63bcda40d8b48df3140fa06f57071c0ad5a3
jemo29 <jemoa30@gmail.com>
2022-05-23 19:45:35 +0400

    New code for  Challenge #1

---
+++ nino_papava/Challenge1/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+  <head >
+     <br><br><h1><strong>Github Users</strong></h1>
+   </head>
+   <link rel="stylesheet" href="app.css">
