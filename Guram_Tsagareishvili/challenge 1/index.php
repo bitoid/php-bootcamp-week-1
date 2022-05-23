7573729fc02976808d3a8e1193773d5d5bb1c70c
Guram Tsagareishvili <Mikkelletoo@gmail.com>
2022-05-23 21:07:16 +0400

    Submitted changes for Week 1 challenge

--- Guram_Tsagareishvili/challenge 1/index.php
+++ Guram_Tsagareishvili/challenge 1/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="index.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>My form</title>
 </head>
@@ -13,12 +14,12 @@
     <form  action="/index.php" method="POST" enctype="multipart/form-data">
     <div class="row mt-2">
         <div class="col">
-        <label for="username">Enter First Name</label>
-        <input class="form-control" id="username" type="text" required name="name" placeholder="First Name">
+        <label for="name">Enter First Name</label>
+        <input class="form-control pattern" id="name" type="text" required name="name" placeholder="First Name" pattern="^[A-Za-z]+$">
         </div >
         <div class="col">
-        <label for="$username1">Enter Last Name</label>
-        <input class="form-control" type="text" required name="surname" id="$username1" placeholder="Last Name">
+        <label for="surname">Enter Last Name</label>
+        <input class="form-control pattern" type="text" required name="surname" id="surname" placeholder="Last Name" pattern="^[A-Za-z]+$">
         </div>
         </div>
         <div class="row">
@@ -30,41 +31,28 @@
         <input  class="btn btn-primary mt-4" type="submit" name="submit">
         </div>
         </div>
-
     </form>
 
-    <?php
-        
-      $pattern = '/^[a-zA-Z]+$/i';
-    if(array_key_exists("submit", $_POST)){
-        if(preg_match_all($pattern, $_POST["name"]) && preg_match_all($pattern, $_POST["surname"]) ){
-
-        ?>
+    <?php  $pattern = '/^[a-zA-Z]+$/i'; 
+    if(array_key_exists("submit", $_POST)):
+        if(preg_match_all($pattern, $_POST["name"]) && preg_match_all($pattern, $_POST["surname"]) ): ?>
         <div class="container text-center">
-       <h2 class="text"><?php echo $_POST["name"] . " " . $_POST["surname"]; ?></h2>
+       <h2 class="text"><?= $_POST["name"] . " " . $_POST["surname"]; ?></h2>
        </div>
-       <?php
-        
-        $temp_loc = $_FILES['img']["tmp_name"];
+
+       <?php $temp_loc = $_FILES['img']["tmp_name"];
         $file_storage = $_FILES["img"]["name"];
+        move_uploaded_file($temp_loc, $file_storage); ?> 
 
-        move_uploaded_file($temp_loc, $file_storage);
-
-        
-        ?> 
         <div class="container text-center w-50 h-50">
-        <img class="img img-fluid"" src="<?php echo $_FILES["img"]["name"] ?>" alt="profile"> 
+        <img class="img img-fluid"" src="<?= $file_storage ?>" alt="profile"> 
         </div>
-        <?php
-        } else {
-           ?>  <div class="container text-center"><h2><?php echo  "User information should only consist from English letters!"; ?> </h2></div> <?php 
-        }
-    }
-    ?> 
 
-   
+        <?php else:?> 
+         <div class="container text-center"><h2><?=  "User information should only consist from English letters!"; ?></h2></div> 
+         <?php endif;
+    endif; ?> 
+
 </main>
-
-
 </body>
 </html>
--- Guram_Tsagareishvili/challenge 2/index.php
+++ Guram_Tsagareishvili/challenge 2/index.php
@@ -8,71 +8,43 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Challenge 2</title>
 </head>
+
 <body>
-
 <div class="container mt-2 text-center">
-  
 <form action="index.php" method="POST">
-
     <label class="form-label"  for="$repos"><h1 class="fs-2">Enter username here to see users Git information<h1></label>
     <input class="form-control"  type="text" name="repos" id="$repos">
     <input class="btn btn-primary mt-2" type="submit" name="submit">
-
 </form>
 </div>
 
-<?php
-
-if(array_key_exists("submit", $_POST)){
-
-$ch = curl_init();
-
-
-$url = "https://api.github.com/users/" . $_POST['repos'] . "/repos";
-
-
-curl_setopt_array($ch,[
-    CURLOPT_URL => $url,
+<?php if(array_key_exists("submit", $_POST)):
+$ch_repos = curl_init();
+$url_repos = "https://api.github.com/users/" . $_POST['repos'] . "/repos";
+curl_setopt_array($ch_repos,[
+    CURLOPT_URL => $url_repos,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => ['User-Agent: PHP']
-
 ]);
+$json_data = curl_exec($ch_repos);
+curl_close($ch_repos);
+$repos_data = json_decode($json_data);
 
-
-$data = curl_exec($ch);
-
-curl_close($ch);
-
-
-$data1 = json_decode($data);
-
-
-    $ch = curl_init();
-    
-    
-    $url = "https://api.github.com/users/" . $_POST['repos'] . "/followers";
-    
-    
-    curl_setopt_array($ch,[
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => ['User-Agent: PHP']
-    
+$ch_follower = curl_init();
+$url_follower = "https://api.github.com/users/" . $_POST['repos'] . "/followers"; 
+curl_setopt_array($ch_follower,[
+      CURLOPT_URL => $url_follower,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HTTPHEADER => ['User-Agent: PHP']
     ]);
-    
-    
-    $data2 = curl_exec($ch);
-    
-    curl_close($ch);
-    
-    
-    $data3 = json_decode($data2);
-    
-    ?>
+$json_data = curl_exec($ch_follower); 
+curl_close($ch_follower);  
+$follower_data = json_decode($json_data);?>
+
   <div class="container">
     <div class="row">
       <div class="col">
-        <h2>Here are <?php echo $_POST['repos']  ?>s Repositories</h2>
+        <h2>Here are <?= $_POST['repos']?>s Repositories</h2>
 <table class="table">
   <thead>
     <tr>
@@ -80,44 +52,38 @@ $data1 = json_decode($data);
       <th scope="col"><h3>Link to Repository</h3></th>
     </tr>
   </thead>
-  <?php foreach($data1 as $data){ ?>
+  <?php foreach($repos_data as $data):?>
         <tbody>
     <tr>
-      <td scope="row"><h4> <?php echo $data -> name ."\n"; ?> </h4></td>
-      <td class="td"><p><a href="<?php echo $data -> html_url ."\n"; ?>"><?php echo $data -> html_url ."\n"; ?></a> </p></td>
+      <td scope="row"><h4> <?= $data -> name ."\n"; ?> </h4></td>
+      <td class="td"><p><a href="<?= $data -> html_url ."\n"; ?>"><?= $data -> html_url ."\n"; ?></a> </p></td>
   </tr>
-  <?php } ?>
+  <?php endforeach; ?>
   </tbody>
   </table>
   </div>
   <div class="col">
-  <h2>Here are <?php echo $_POST['repos']  ?>s Followers</h2>
+  <h2>Here are <?= $_POST['repos']  ?>s Followers</h2>
   <table class="table">
   <thead>
     <tr>
       <th scope="col"><h3>Followers</h3></th>
-      <th scope="col"><h3>Follower Avatars</h3></th>
+      <th scope="col"><h3>Avatar</h3></th>
     </tr>
   </thead>
   <tbody>
-  <?php foreach($data3 as $data){ ?>
+  <?php foreach($follower_data as $data):?>
     <tr>
-      <td class="td"> <h4> <?php echo $data -> login ."\n"; ?> </h4></td>
-      <td class="td"><a href="<?php echo $data -> html_url;  ?>"><img src="<?php echo $data -> avatar_url;  ?>" class="img-fluid" alt="NAN"></a></td>
+      <td class="td"> <h4> <?= $data -> login ."\n"; ?> </h4></td>
+      <td class="td"><a href="<?= $data -> html_url;  ?>"><img src="<?= $data -> avatar_url;?>" class="img-fluid" alt="NAN"></a></td>
     </tr>
   </tbody>
-        <?php
-       } 
-    echo '</pre>'; 
-    
-}
-?>
-</table>
+        <?php endforeach; ?>
+        </table>
 </div>
 </div>
 </div>
+<?php endif?>
 
-
-    
 </body>
 </html>
