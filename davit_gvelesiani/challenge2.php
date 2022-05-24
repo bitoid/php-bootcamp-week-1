3e4c16f2bce833a52234351eaa80558289585426
DavitGvelesiani <98581644+DavitGvelesiani@users.noreply.github.com>
2022-05-24 14:23:38 +0400

    update challenge2

--- davit_gvelesiani/challenge2.php
+++ davit_gvelesiani/challenge2.php
@@ -9,68 +9,64 @@
     <form method="POST">        
         <input type="text" name="userName" placeholder="user name">
         </br>
-        </br>
         <input type="checkbox" name="followers">followers</input>
         <input type="checkbox" name="repos">repositories</input>
         <input type="submit" name="submit" />
     </form>
 
 
-<?php
-    if(isset($_POST['submit'])){
-       $userName=$_POST['userName'];
-        $url="https://api.github.com/users/$userName/repos";
-        $param=[
-            'http' => [
-                'method' => 'GET',
-                'header' => [
-                    'User-Agent: PHP'
-                ]
-            ]
-        ];
+  <?php
+      if(isset($_POST['submit'])){
+         $userName=$_POST['userName'];
+          $repos_url="https://api.github.com/users/$userName/repos";
+          $repos_param=[
+              'http' => [
+                  'method' => 'GET',
+                  'header' => [
+                      'User-Agent: PHP'
+                  ]
+              ]
+          ];
 
-        $json = file_get_contents($url, false, stream_context_create($param));
-        $data = json_decode($json, false);
-        
-        echo "<h1>Repos:</h1>";
-        
-        if($_POST['repos']){
-            foreach ($data as $user) {           
-            echo "name: ".$user->name;
-            echo "<br />";
-            echo "<a href='$user->html_url'>Click here to view</a>";
-            echo "<br /> <br />";
-            }
-        }
-        
+          $repos_json = file_get_contents($repos_url, false, stream_context_create($repos_param));
+          $repos_data = json_decode($repos_json, false);
 
-        $url="https://api.github.com/users/$userName/followers";
-        $param=[
-            'http' => [
-                'method' => 'GET',
-                'header' => [
-                    'User-Agent: PHP'
-                ]
-            ]
-        ];
+          echo "<h1>Repos:</h1>";
 
-        $json = file_get_contents($url, false, stream_context_create($param));
-        $data = json_decode($json, false);
+          if($_POST['repos']){
+              foreach ($repos_data as $user) {           
+              echo "name: ".$user->name;
+              echo "<br />";
+              echo "<a href='$user->html_url'>Click here to view</a>";
+              echo "<br /> <br />";
+              }
+          }
 
-        echo "<h1>Followers:</h1>";
-        
-        if($_POST['followers']){
-            foreach ($data as $user) {        
-            echo "name: ".$user->login;
-            echo "<br />";
-            echo "<a href='$user->html_url'><img src='$user->avatar_url'/></a>";
-            echo "<br /> <br />";
-            } 
-        }
-        
-    }
-    
 
-?>
+          $followers_url="https://api.github.com/users/$userName/followers";
+          $followers_param=[
+              'http' => [
+                  'method' => 'GET',
+                  'header' => [
+                      'User-Agent: PHP'
+                  ]
+              ]
+          ];
+
+          $followers_json = file_get_contents($followers_url, false, stream_context_create($followers_param));
+          $followers_data = json_decode($followers_json, false);
+
+          echo "<h1>Followers:</h1>";
+
+          if($_POST['followers']){
+              foreach ($followers_data as $user) {        
+              echo "name: ".$user->login;
+              echo "<br />";
+              echo "<a href='$user->html_url'><img src='$user->avatar_url'/></a>";
+              echo "<br /> <br />";
+              } 
+          }
+      }
+  ?>
 
 </html>
