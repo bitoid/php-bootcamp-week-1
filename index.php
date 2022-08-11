77287266ec69bf5525221a09303cc3854bad9a41
Dame <dame@Dames-MacBook-Air.local>
2022-08-11 14:41:50 +0400

    here is my important files and folders for challenge of week 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <form method="POST" action="validate.php" enctype="multipart/form-data">
+      <input type="text" name="firstname" required>
+      <input type="text" name="lastname" required>
+      <input type="file" name="image" required>
+      <input type="submit" name="submit" required>
+    </form>
+  </body>
+</html>
