3e77267af0da4a4acb90067efda93cd979ddbea0
DavitGe <65369123+DavitGe@users.noreply.github.com>
2022-08-12 04:05:29 +0400

    Davit Gelovani

---
+++ davit_gelovani/index.php
@@ -0,0 +1,14 @@
+<!doctype HTML>
+<html>
+    <head> 
+
+    </head>
+
+    <form action="/upload.php" method="POST" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="Enter your name:" />
+        <input type="text" name="surname" placeholder="Enter your surname:" />
+        <input type="submit" name="submit" />
+    </form> 
+
+  
+</html>
---
+++ davit_gelovani/upload.php
@@ -0,0 +1,14 @@
+<?php 
+if(isset($_POST['submit']) && isset($_FILES['profile']) && $_POST['name'] && $_POST['surname']){
+  
+  
+  print '<h1>' . 'Name:' .'</h1>' . $_POST['name'];
+  print '<h1>' . 'Surname:' .'</h1>' . $_POST['surname'];
+  print '<h1>Profile Picture:</h1>';  
+
+  $imageData = file_get_contents($_FILES['profile']['tmp_name']); 
+  echo sprintf('<img src="data:image/png;base64,%s" />', base64_encode($imageData));
+
+}  else{
+  header("Location: index.php");
+}
