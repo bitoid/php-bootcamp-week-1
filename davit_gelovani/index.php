cc46c936c46fe7f236dcbe6fe4ce5030f7393452
DavitGe <65369123+DavitGe@users.noreply.github.com>
2022-08-12 04:23:08 +0400

    Davit Gelovani

--- davit_gelovani/index.php
+++ davit_gelovani/index.php
@@ -7,6 +7,7 @@
             <h1>Fill the form:</h1>
             <input type="text" name="name" placeholder="Enter your name:" />
             <input type="text" name="surname" placeholder="Enter your surname:" />
+            <input type="file" name="profile" />
             <input type="submit" name="submit" />
         </form> 
     </body>
