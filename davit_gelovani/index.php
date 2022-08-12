89bce44ae88653551a52f7292b83953daf9775d4
DavitGe <65369123+DavitGe@users.noreply.github.com>
2022-08-12 04:22:08 +0400

    Davit Gelovani

--- davit_gelovani/index.php
+++ davit_gelovani/index.php
@@ -4,6 +4,7 @@
     </head>
     <body>
         <form action="/upload.php" method="POST" enctype="multipart/form-data">
+            <h1>Fill the form:</h1>
             <input type="text" name="name" placeholder="Enter your name:" />
             <input type="text" name="surname" placeholder="Enter your surname:" />
             <input type="submit" name="submit" />
