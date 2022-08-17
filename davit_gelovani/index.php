7703bfbeae2ed257cd2cc2d3b5d2c871b2f89d53
Davit <davitge303@gmail.com>
2022-08-17 22:46:02 +0400

    Davit Gelovani

--- davit_gelovani/index.php
+++ davit_gelovani/index.php
@@ -1,14 +1,28 @@
 <!doctype HTML>
 <html>
     <head> 
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Challange 1</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     </head>
-    <body>
+    <body class="container">
         <form action="/upload.php" method="POST" enctype="multipart/form-data">
-            <h1>Fill the form:</h1>
-            <input type="text" name="name" placeholder="Enter your name:" />
-            <input type="text" name="surname" placeholder="Enter your surname:" />
-            <input type="file" name="profile" />
-            <input type="submit" name="submit" />
+                <legend>Fill the form:</legend>
+                <div class="mb-3">
+                  <label for="nameInput" class="form-label">Name</label>
+                  <input type="text" name="name" class="form-control" id="nameInput" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <label for="surnameInput" class="form-label">Surname</label>
+                  <input type="text" name="surname" class="form-control" id="surnameInput" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <label for="formFile" class="form-label">Upload your profile picture</label>
+                  <input class="form-control" name="profile" type="file" id="formFile">
+                </div>
+
+                <input type="submit" name="submit" class="btn btn-success" />
         </form> 
     </body>
   
--- davit_gelovani/upload.php
+++ davit_gelovani/upload.php
@@ -1,12 +1,28 @@
-<?php 
-if(isset($_POST['submit']) && isset($_FILES['profile']) && $_POST['name'] && $_POST['surname']){
-  print '<h1>' . 'Name:' .'</h1>' . $_POST['name'];
-  print '<h1>' . 'Surname:' .'</h1>' . $_POST['surname'];
-  print '<h1>Profile Picture:</h1>';  
+<!doctype HTML>
+<html>
+    <head> 
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Challange 1</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    </head>
+    <body class="container">
+          <?php 
+          if(isset($_POST['submit']) && isset($_FILES['profile']) && $_POST['name'] && $_POST['surname']){
+            if(ctype_alpha($_POST['name']) || ctype_alpha($_POST['surname'])){
+              print '<h1>' . 'Name:' .'</h1>' . $_POST['name'];
+              print '<h1>' . 'Surname:' .'</h1>' . $_POST['surname'];
+              print '<h1>Profile Picture:</h1>'; 
+            }else{
+              echo "<div class='alert alert-danger'>Name and surname should be written with letters A-z</div>";
+            }
+            echo '<h1>Image</h1>';
+            $imageData = file_get_contents($_FILES['profile']['tmp_name']); 
+            echo sprintf('<img src="data:image/png;base64,%s" />', base64_encode($imageData));
 
-  $imageData = file_get_contents($_FILES['profile']['tmp_name']); 
-  echo sprintf('<img src="data:image/png;base64,%s" />', base64_encode($imageData));
+          }  else{
+            header("Location: index.php");
+          }?>
 
-}  else{
-  header("Location: index.php");
-}
+    </body>
+</html>
